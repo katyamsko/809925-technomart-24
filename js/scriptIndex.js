1c3e0719b5c81f2de66152341b38896986c16ed5
katyamsko <46845648+katyamsko@users.noreply.github.com>
2019-02-28 20:16:24 +0300

    защита

--- js/scriptIndex.js
+++ js/scriptIndex.js
@@ -119,3 +119,70 @@ window.addEventListener("keydown", function (evt) {
     }
   }
 });
+
+/*modal-cart*/
+
+var cartBuy = document.querySelectorAll(".buy");
+var popupCart = document.querySelector(".modal-cart");
+var cartClose = popupCart.querySelector(".modal-cart-close");
+var continueShopping = document.querySelector(".modal-more-shopping");
+
+var popupBuy = document.querySelector(".modal-cart-link");
+var quantity = document.querySelector(".quantity").innerHTML;
+
+var bookmark = document.querySelectorAll(".bookmark");
+var quantityBookmark = document.querySelector(".amount-bookmarks").innerHTML;
+
+var cartLink = document.querySelector(".index-cart");
+var bookmarkLink = document.querySelector(".header-bookmarks");
+
+if (quantityBookmark > 0) {
+  bookmarkLink.classList.add("not-empty");
+}
+
+if (quantity > 0) {
+  cartLink.classList.add("not-empty");
+}
+
+for (var i = 0; i < cartBuy.length; i++) {
+  cartBuy[i].addEventListener("click", function (evt) {
+    evt.preventDefault();
+    quantity ++;
+    if (quantity > 0) {
+      cartLink.classList.add("not-empty");
+    }
+    document.querySelector(".quantity").innerHTML = quantity;
+    popupCart.classList.add("modal-show");
+  });
+   cartClose.addEventListener("click", function (evt) {
+   evt.preventDefault();
+   popupCart.classList.remove("modal-show");
+  });
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (popupCart.classList.contains("modal-show")) {
+        popupCart.classList.remove("modal-show");
+      }
+    }
+  });
+}
+    
+continueShopping.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popupCart.classList.remove("modal-show");
+})
+
+console.log(quantityBookmark);
+for (var i = 0; i < bookmark.length; i ++) {
+  bookmark[i].addEventListener("click", function(evt) {
+    evt.preventDefault();
+    quantityBookmark++;
+    if (quantityBookmark > 0) {
+      bookmarkLink.classList.add("not-empty");
+    }
+    document.querySelector(".amount-bookmarks").innerHTML = quantityBookmark;
+  })
+}
+
